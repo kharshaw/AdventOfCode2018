ee7fb68088a345f20558a74a7257f0f751fc04c0
kharshaw <keith.harshaw@gmail.com>
2018-12-05 10:33:04 -0500

    code complete solution.  needs clean up.

--- aoc-day1/src/Program.fs
+++ aoc-day1/src/Program.fs
@@ -1,8 +1,25 @@
 ﻿// Learn more about F# at http://fsharp.org
 
 open System
+open System.IO
+
+let (|Int|_|) (str : String) =
+  match Int32.TryParse(str) with
+  | (true,int) -> Some(int)
+  | _ -> None
+
+let parseFrequency line =
+  match line with
+  | Int i -> i
+  | _ -> 0
+
+let readFrequencies filename =
+  filename |> File.ReadLines |> Seq.map parseFrequency
 
 [<EntryPoint>]
 let main argv =
-    printfn "Hello World from F#!"
+    let frequencies = readFrequencies @"data\input.txt"
+    let net = Seq.sum(frequencies)
+
+    printfn "Sum: %A" net
     0 // return an integer exit code
