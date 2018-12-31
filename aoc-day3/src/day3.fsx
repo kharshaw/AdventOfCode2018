b69fad23b2297fab1468d185e5c01aa8d811270a
kharshaw <keith.harshaw@gmail.com>
2018-12-31 16:09:27 -0500

    solved day 2 part 2

--- aoc-day3/src/day3.fsx
+++ aoc-day3/src/day3.fsx
@@ -16,7 +16,7 @@ let parseRawClaim (claimData:string) = (
                               .Replace("x",",")
   let valuesArray = valuesString.Split(',') |> Array.map (fun s -> int s)
 
-  let claim = {Id = valuesArray.[0]; X = valuesArray.[1]; Y = valuesArray.[2]; Height = valuesArray.[3]; Width = valuesArray.[4]}
+  let claim = {Id = valuesArray.[0]; X = valuesArray.[1]; Y = valuesArray.[2]; Height = valuesArray.[4]; Width = valuesArray.[3]}
 
   claim
   )
@@ -54,4 +54,31 @@ let addAllPlacedClaims claims =
  (Seq.head claims, Seq.skip 1 claims) ||> Seq.fold (combinedPlacedClaims)
 
 
-claims |> claimLocations clothWidth clothHeight |> addAllPlacedClaims |> Seq.filter (fun c -> c > 1) |> Seq.length
+let claims' = claims |> claimLocations clothWidth clothHeight
+
+let cloth =  claims' |> addAllPlacedClaims
+
+
+let isNoOverlapMatch claimPositions cloth =
+  Seq.map2 (fun claim cloth ->
+    match (claim, cloth) with
+    | (1,1) -> true
+    | (0,_) -> true
+    | (_,_) -> false
+  ) claimPositions cloth
+
+
+let part1 clothWithClaims =
+  cloth |> Seq.filter (fun c -> c > 1) |> Seq.length
+
+
+let part2 cloth claims  =
+  let overlapMap = claims |> Seq.map (fun claim -> isNoOverlapMatch claim cloth  )
+  let firstNonOverlap = overlapMap |> Seq.findIndex (fun map -> Seq.forall (fun el -> el = true) map)
+  firstNonOverlap
+
+
+
+part1 cloth
+
+part2 cloth claims'
